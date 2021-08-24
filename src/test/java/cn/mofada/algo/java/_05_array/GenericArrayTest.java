bad89c7ad9e66300031d3b41691c940fcead39ab
fada <fada@mofada.cn>
2021-08-25 00:37:22 +0800

    1.完成java 数组 2.todo kotlin数组

--- src/test/java/cn/mofada/algo/java/_05_array/GenericArrayTest.java
+++ src/test/java/cn/mofada/algo/java/_05_array/GenericArrayTest.java
@@ -5,6 +5,9 @@ import org.junit.jupiter.api.Test;
 public class GenericArrayTest {
     @Test
     public void add() {
+        // 这里测试所以用的int类型
+        // 如果数据用于存储基本数据类型，建议使用数组，不适用列表
+        // 因为每次会进行Autoboxing和Unboxing
         GenericArray<Integer> array = new GenericArray<Integer>(10);
 
         array.add(0, 5);
