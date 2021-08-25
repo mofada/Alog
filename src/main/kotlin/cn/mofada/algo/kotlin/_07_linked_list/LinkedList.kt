c17c76f5635bfe2cf6fec1cb2d77e6fb5d06ba01
fada <fada@mofada.cn>
2021-08-26 00:51:04 +0800

    1.链表

--- src/main/kotlin/cn/mofada/algo/kotlin/_07_linked_list/LinkedList.kt
+++ src/main/kotlin/cn/mofada/algo/kotlin/_07_linked_list/LinkedList.kt
@@ -6,7 +6,48 @@ package cn.mofada.algo.kotlin._07_linked_list
  * 3)两个有序链表合并
  * 4)删除链表倒数第n个节点
  * 5)求链表的中间节点
+ * //todo 链表太难了吧,先看后面的
  */
-class LinkedList {
+object LinkedList {
+    /**
+     * 单链表反转
+     */
+    fun <T> reverse(list: Node<T>?): Node<T>? {
+        var current = list
+        var previous: Node<T>? = null
 
+        while (current != null) {
+            //首先把下一个的值拿出来
+            val next = current.next
+            //然后把当前的next的指向上一个
+            current.next = previous
+            //把当前赋值给上一个,在这里换位置
+            previous = current
+            //当前的换到下一个的值,这里就完成了位置交换
+            current = next
+        }
+
+        return previous
+    }
+
+    /**
+     * 检测环
+     */
+    fun <T> checkCircle(list: Node<T>?): Boolean {
+        if (list == null) return false
+
+        var fast = list.next
+        var slow = list
+
+        while (fast != null && slow != null) {
+            fast = fast.next?.next
+            slow = slow.next
+
+            if(slow == fast) return true
+        }
+
+        return false
+    }
 }
+
+data class Node<T>(var data: T, var next: Node<T>?)
