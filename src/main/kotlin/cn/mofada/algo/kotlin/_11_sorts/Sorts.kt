30513604701a6ff5627efe90fb3682b15e7423a4
fada <fada@mofada.cn>
2021-08-31 00:27:02 +0800

    1.归并排序、插入排序

--- src/main/kotlin/cn/mofada/algo/kotlin/_11_sorts/Sorts.kt
+++ src/main/kotlin/cn/mofada/algo/kotlin/_11_sorts/Sorts.kt
@@ -1,6 +1,6 @@
 package cn.mofada.algo.kotlin._11_sorts
 
-import kotlin.math.min
+import kotlin.random.Random
 
 /**
  * 冒泡排序、插入排序、选择排序
@@ -94,12 +94,128 @@ object Sorts {
 
         return array
     }
+
+    /**
+     * 归并排序
+     * 空间复杂度：O(n),不是原地排序
+     * 时间复杂度：O(n log n)
+     * 不是稳定排序算法
+     */
+    fun mergeSort(array: Array<Int>, left: Int, right: Int) {
+        if (left >= right) return
+
+        //取中间点
+        val q = (left + right) / 2
+
+        mergeSort(array, left, q)
+        mergeSort(array, q + 1, right)
+        merge(array, left, q, right)
+    }
+
+    /**
+     * 数据合并
+     */
+    private fun merge(array: Array<Int>, left: Int, q: Int, right: Int) {
+        //创建左右的数组，+2是因为要放q和哨兵
+        val arrayLeft = Array<Int>(q - left + 2) { 0 }
+        //+1是因为只要放哨兵
+        val arrayRight = Array<Int>(right - q + 1) { 0 }
+
+        //左边数据数据放进去
+        for (i in 0..q - left) {
+            arrayLeft[i] = array[left + i]
+        }
+        //添加哨兵
+        arrayLeft[q - left + 1] = Int.MAX_VALUE
+
+        for (i in 0 until right - q) {
+            arrayRight[i] = array[q + i + 1]
+        }
+        //添加哨兵
+        arrayRight[right - q] = Int.MAX_VALUE
+
+        // 左边数组的下标
+        var i = 0
+        //右边数组的下标
+        var j = 0
+        var k = left
+
+        while (k <= right) {
+            if (arrayLeft[i] <= arrayRight[j]) {
+                array[k++] = arrayLeft[i++]
+            } else {
+                array[k++] = arrayRight[j++]
+            }
+        }
+    }
+
+    /**
+     * 快速排序
+     * 空间复杂度：O(n),是原地排序
+     * 时间复杂度：O(n log n)
+     * 不是稳定排序算法
+     */
+    fun quickSort(array: Array<Int>, left: Int, right: Int) {
+        if (left >= right) return
+
+        //取中间点
+        val q = partition(array, left, right)
+        quickSort(array, left, q - 1)
+        quickSort(array, q + 1, right)
+    }
+
+    /**
+     * 分区函数
+     */
+    fun partition(array: Array<Int>, left: Int, right: Int): Int {
+        val middle = (left + right) / 2
+        val pivot = array[middle]
+
+        //交换到右边
+        var tmp = array[right]
+        array[right] = pivot
+        array[middle] = tmp
+
+        var i = left
+        for (j in left until right) {
+            if (array[j] < pivot) {
+                if (i == j) {
+                    i++
+                } else {
+                    tmp = array[i]
+                    array[i++] = array[j]
+                    array[j] = tmp
+                }
+            }
+        }
+
+        tmp = array[i]
+        array[i] = array[right]
+        array[right] = tmp
+
+        return i
+    }
 }
 
 fun main() {
-    val array = arrayOf(5, 7, 1, 9, 2, 3, 4, 5, 6)
+    val random = Random(100)
+    val array = Array(1000000) {
+        random.nextInt(1000000)
+    }
+    val starter = System.currentTimeMillis()
+    Sorts.mergeSort(array, 0, array.lastIndex)
+//    Sorts.quickSort(array, 0, array.lastIndex)
+//    Sorts.bubbleSort(array)
+//    Sorts.selectionSort(array)
+    val end = System.currentTimeMillis()
 
-    Sorts.selectionSort(array)
+    println(array.joinToString("\n"))
 
-    println(array.joinToString(","))
+    println()
+    println((end - starter))
+
+//    val array = arrayOf(6,11,3,9,8)
+//    Sorts.quickSort(array, 0, array.size - 1)
+
+//    println(array.joinToString("\n"))
 }
